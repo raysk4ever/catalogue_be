ef3800a552e25788658707d968eb465844a6c6cb
Ravi Kant Singh <raysk7161@gmail.com>
2020-02-02 18:14:51 +0530

    added filter in sidebars

--- controllers/product.js
+++ controllers/product.js
@@ -1,4 +1,6 @@
 const { Product, validateProduct } = require('../models/product');
+const {Brand} = require('../models/brand');
+const { Category } = require('../models/category');
 
 const addNewProduct = async (req, res) => {
     try{
@@ -16,7 +18,11 @@ const addNewProduct = async (req, res) => {
 
 const getAllProducts = async (req, res) =>{
     try{
-        let products = await Product.find().sort("name");
+        let filterOption = {};
+            if(req.query.brands) filterOption['brand'] = req.query.brands
+            if(req.query.categories) filterOption['parentCategory'] = req.query.categories
+
+        let products = await Product.find(filterOption).sort("-createdAt").populate("brand parentCategory");
         res.send({status: true, error: false, products});
     }catch(ex){
         return res.send({status: false, error: true, errorMessage: `Something went wrong. Error: ${ex}`});
--- models/product.js
+++ models/product.js
@@ -1,26 +1,27 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
-Joi.objectId = require('joi-objectid');
+Joi.objectId = require('joi-objectid')(Joi);
 
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true },
     brand: { type: mongoose.Schema.Types.ObjectId, ref: "brand" },
     parentCategory: { type: mongoose.Schema.Types.ObjectId, ref: "category" },
     subCategory: { type: String, default: "" },
-    breadCrumb: { type: String, default: "" },
-    Specifications: { type: mongoose.Schema.Types.Mixed, default: '' }
+    breadcrumb: { type: String, default: "" },
+    specifications: { type: mongoose.Schema.Types.Mixed, default: '' },
+    createdAt: { type: Date, default: Date.now() }
 });
 
 const Product = mongoose.model('product', productSchema);
 
 const validateProduct = product => {
     const Schema = {
-        name: Joi.string().require(),
+        name: Joi.string().required(),
         brand: Joi.objectId(),
         parentCategory: Joi.objectId(),
         subCategory: Joi.string(),
-        breadCrumb: Joi.string(),
-        Specifications: Joi.any(),
+        breadcrumb: Joi.string(),
+        specifications: Joi.any(),
     }
     return Joi.validate(product, Schema);
 }
